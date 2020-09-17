62111f4c8dd7de3fa910f29cea01b5d1818c8da0
Valdimax <valdimarmp20@ru.is>
2020-09-17 11:10:40 +0000

    nyjar attir

--- tiletraveller.py
+++ tiletraveller.py
@@ -3,6 +3,17 @@ south = "s"
 west  = "w"
 east = "e"
 
+def direction (n, s, w, e):
+    message= "You can travel:"
+    if n==1:
+        message +="(N)orth"
+    elif s==1:
+        message += "(S)outh"
+    elif w==1:
+        message += "(W)est"
+    elif e==1:
+        message += "(E)ast"
+        
 
 
 print("You can travel: (N)orth.")
