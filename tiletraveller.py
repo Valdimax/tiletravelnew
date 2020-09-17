95ffd9615428c2eaa42245e2210fd89d6a6ab064
sigurgeir smári stephensen <sigurgeir.stephensen@gmail.com>
2020-09-17 11:58:44 +0000

    coolman

--- tiletraveller.py
+++ tiletraveller.py
@@ -1,22 +1,42 @@
-north= "n"
-south = "s"
-west  = "w"
-east = "e"
+
+x = 1 
+y = 1 
+location = [x, y]
+
+
 
 def direction (n, s, w, e):
     message= "You can travel:"
     if n==1:
         message +="(N)orth"
+    if e== 1 and n==1:
+        message += " or (E)ast"
+    if w==1 and e==1 :
+        message += "or (W)est"
     elif s==1:
         message += "(S)outh"
-    elif w==1:
-        message += "(W)est"
     elif e==1:
         message += "(E)ast"
         
 
+# x = 2
+# y= 2 
+#  n e w s
+# (0,0,0,1,)
+def userinput():
 
-print("You can travel: (N)orth.")
-
-print ("change")
-print("Not a valid direction!")
+while location != [3,1]:
+    if location [1,1] or location[2,1]:
+        n,e,w,s = (1,0,0,0)
+    elif location [1,2]:
+        n,e,w,s = (1,1,0,1)
+    elif location [2,2] or location[3,2]:
+        n,e,w,s = (0,0,1,1)
+    elif location [1,3]:
+        n,e,w,s = (0,1,0,1)
+    elif location [2,3]:
+        n,e,w,s = (0,1,1,0)
+    elif location [3,3]:
+        n,e,w,s = (0,0,1,1)
+    elif location [3,1]:
+        print("Victory!")
