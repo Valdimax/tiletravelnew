e377174eea78ab7365b1530d6f7dc5e43c822609
sigurgeir smári stephensen <sigurgeir.stephensen@gmail.com>
2020-09-17 14:20:34 +0000

    doner

--- tiletraveller.py
+++ tiletraveller.py
@@ -1,83 +1,60 @@
 
 
-x, y = 1 , 1
+char_x, char_y = 1, 1
+path = ""
+valid_direction = 'n'
 
-location = [x, y]
-
-def direction (n, e, w, s):
-    message = "You can travel: "
-    if n==1:
-        message += "(N)orth"
-    if e == 1 and n == 1:
-        message += " or (E)ast"
-    elif e == 1 and n !=1:
-        message +="(E)ast"
-    if s== 1 and (n==1 or e==1):
-        message += " or (S)outh"
-    elif s == 1 and not (n==1 or e==1):
-        message += "(S)outh"
-    if w == 1 and (n==1 or e==1 or s==1):
-        message += " or (W)est"
-    elif w==1 and not (n==1 or e==1 or s==1):
-        message += "(W)est"
-    print (message + ".")
-    
-def userinput():
-    move_in_grid= input("Direction: ")
-
-    move_in_grid = move_in_grid.lower()
-
-    return move_in_grid
-
-
-
-
-def move (z, q, x, y):
-    z = z.lower()
-    if z == "n" and n ==1:
-        y +=1
-    elif z == "s" and s ==1:
-        y -=1
-    elif z == "w" and w ==1:
-        x -=1
-    elif z == "e" and e ==1:
-        x +=1
+# foll
+def mover(my_dir, x, y):
+    if ((my_dir == 'n') and ('n' in valid_direction)):
+        y += 1
+    elif ((my_dir == 's') and ('s' in valid_direction)):
+        y -= 1
+    elif ((my_dir == 'e') and ('e' in valid_direction)):
+        x += 1
+    elif ((my_dir == 'w') and ('w' in valid_direction)):
+        x -= 1
     else:
         print("Not a valid direction!")
-    q = [x, y]
-    return q, x, y
+        travel_agree = 0
+    return char_x, char_y
 
-def movement(th , sj, ak, uh, x, y, xy):
-    direction(n, e, w, s)
-    location2 = xy
-    while location2 == xy:
-        way = userinput()
-        xy , x, y = move(way, xy, x, y)
-    return xy,x,y 
 
-victory = [3,1]
 
-while location != victory:
-    if location == [1,1] or location == [2,1]:
-        n, e, w, s = 1, 0, 0, 0
-        location,x,y = movement(n,e,w,s,x,y,location)
-    if location == [1,2]:
-        n, e, w, s = 1, 1, 0, 1
-        location,x,y = movement(n,e,w,s,x,y,location)
-    if location ==[2,2] or location == [3,2]:
-        n, e, w, s = 0, 0, 1, 1
-        location,x,y = movement(n,e,w,s,x,y,location)
-    if location == [1,3]:
-        n, e, w, s = 0, 1, 0, 1
-        location,x,y = movement(n,e,w,s,x,y,location)
-    if location ==  [2,3]:
-        n, e, w, s = 0, 1, 1, 0
-        location,x,y = movement(n,e,w,s,x,y,location)
-    if location ==  [3,3]:
-        n, e, w, s = 0, 0, 1, 1
-        location,x,y = movement(n,e,w,s,x,y,location)
+# Rooms
+room1_1 = "(N)orth."
+room1_2 = "(N)orth or (E)ast or (S)outh." 
+room1_3 = "(E)ast or (S)outh."
+room2_1 = "(N)orth."
+room2_2 = "(S)outh or (W)est."
+room2_3 = "(E)ast or (W)est."
+room3_1 = "(N)orth."
+room3_2 = "(N)orth or (S)outh."
+room3_3 = "(S)outh or (W)est."
+path = room1_1
+
+travel_agree = 1
+
+while 1:
     
-print("Victory!")
+    if travel_agree == 1:
+        print("You can travel: " + path)
+
+    path = ''
+    direction = input("Direction: ").lower()
+    travel_agree = 1
+    
+    char_x, char_y = mover(direction, char_x, char_y)
+
+    if char_x == 1 and char_y == 1: path = room1_1; valid_direction = 'n'
+    elif char_x == 1 and char_y == 2: path = room1_2; valid_ndirection = 'nes'
+    elif char_x == 1 and char_y == 3: path = room1_3; valid_direction = 'es'
+    elif char_x == 2 and char_y == 1: path = room2_1; valid_direction = 'n'
+    elif char_x == 2 and char_y == 2: path = room2_2; valid_direction = 'ws'
+    elif char_x == 2 and char_y == 3: path = room2_3; valid_direction = 'we'
+    elif char_x == 3 and char_y == 1: path = room3_1; print("Victory!"); break
+    elif char_x == 3 and char_y == 2: path = room3_2; valid_direction = 'ns'
+    elif char_x == 3 and char_y == 3: path = room3_3; valid_direction = 'ws'
 
 
 
