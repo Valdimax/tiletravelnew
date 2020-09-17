0dacd871cb6bc1459413199760369ffd34c73b94
sigurgeir smári stephensen <sigurgeir.stephensen@gmail.com>
2020-09-17 13:50:20 +0000

    finished

--- tiletraveller.py
+++ tiletraveller.py
@@ -1,65 +1,83 @@
 
-x = 1 
-y = 1 
+
+x, y = 1 , 1
+
 location = [x, y]
 
-def move (x, y, z, q):
+def direction (n, e, w, s):
+    message = "You can travel: "
+    if n==1:
+        message += "(N)orth"
+    if e == 1 and n == 1:
+        message += " or (E)ast"
+    elif e == 1 and n !=1:
+        message +="(E)ast"
+    if s== 1 and (n==1 or e==1):
+        message += " or (S)outh"
+    elif s == 1 and not (n==1 or e==1):
+        message += "(S)outh"
+    if w == 1 and (n==1 or e==1 or s==1):
+        message += " or (W)est"
+    elif w==1 and not (n==1 or e==1 or s==1):
+        message += "(W)est"
+    print (message + ".")
+    
+def userinput():
+    move_in_grid= input("Direction: ")
+
+    move_in_grid = move_in_grid.lower()
+
+    return move_in_grid
+
+
+
+
+def move (z, q, x, y):
     z = z.lower()
     if z == "n" and n ==1:
         y +=1
-    elif z == "s" and s==1:
-        y-=1
-    elif z == "w" and w==1:
+    elif z == "s" and s ==1:
+        y -=1
+    elif z == "w" and w ==1:
         x -=1
-    elif e == "e" and e==1:
+    elif z == "e" and e ==1:
         x +=1
     else:
         print("Not a valid direction!")
-    q=[x, y]
+    q = [x, y]
     return q, x, y
 
+def movement(th , sj, ak, uh, x, y, xy):
+    direction(n, e, w, s)
+    location2 = xy
+    while location2 == xy:
+        way = userinput()
+        xy , x, y = move(way, xy, x, y)
+    return xy,x,y 
 
+victory = [3,1]
 
-def direction (n, s, w, e):
-    message= "You can travel:"
-    if n==1:
-        message +="(N)orth"
-    if n==1 and e==1 and s==1:
-        message +="or (E)ast or (S)outh"
-    if s== 1 and e==1:
-        message += "(E)ast or (S)outh"
-    if w==1 and e==1 :
-        message += "(E)ast or (W)est"
-    if w==1 and s==1 :
-        message += "(S)outh or (W)est"
-    if n==1 and s==1 :
-        message += "or (S)outh"
-    
-def userinput():
-    move_in_grid= input("direction: ")
-    move_in_grid = move_in_grid.lower()
-    return move_in_grid
-
-# x = 2
-# y= 2 
-#  n e w s
-# (0,0,0,1,)
-
-while location != [3,1]:
+while location != victory:
     if location == [1,1] or location == [2,1]:
-        n,e,w,s = (1,0,0,0)
-    elif location == [1,2]:
-        n,e,w,s = (1,1,0,1)
-    elif location ==[2,2] or location == [3,2]:
-        n,e,w,s = (0,0,1,1)
-    elif location == [1,3]:
-        n,e,w,s = (0,1,0,1)
-    elif location ==  [2,3]:
-        n,e,w,s = (0,1,1,0)
-    elif location ==  [3,3]:
-        n,e,w,s = (0,0,1,1)
-    elif location == [3,1]:
-        print("Victory!")
+        n, e, w, s = 1, 0, 0, 0
+        location,x,y = movement(n,e,w,s,x,y,location)
+    if location == [1,2]:
+        n, e, w, s = 1, 1, 0, 1
+        location,x,y = movement(n,e,w,s,x,y,location)
+    if location ==[2,2] or location == [3,2]:
+        n, e, w, s = 0, 0, 1, 1
+        location,x,y = movement(n,e,w,s,x,y,location)
+    if location == [1,3]:
+        n, e, w, s = 0, 1, 0, 1
+        location,x,y = movement(n,e,w,s,x,y,location)
+    if location ==  [2,3]:
+        n, e, w, s = 0, 1, 1, 0
+        location,x,y = movement(n,e,w,s,x,y,location)
+    if location ==  [3,3]:
+        n, e, w, s = 0, 0, 1, 1
+        location,x,y = movement(n,e,w,s,x,y,location)
+    
+print("Victory!")
 
 
 
